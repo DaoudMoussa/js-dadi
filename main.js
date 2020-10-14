1ff7f9d38f8fee849bf079357c9fd12a2548c7da
Daoud <99m.daoud@gmail.com>
2020-10-14 19:27:56 +0200

    Modificato modalita'di raccolta lanci con array e for

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const minValue = 1;
 const maxValue = 6;
+var dadi = [];
 
-// Generare un numero casuale tra 1 e 6 per simulare il lancio di un dado (giocata 1).
-var dado1 = Math.floor(Math.random() * (maxValue - minValue)) + minValue
-document.getElementById('roll-datap1').innerHTML = dado1;
+// Generare 2 numeri casuali tra 1 e 6 per simulare il lancio di 2 dadi.
+for (var i = 0; i < 2; i++) {
+    dadi.push(Math.floor(Math.random() * (maxValue - minValue)) + minValue);
+    console.log(dadi[i]);
+}
 
-// Generare un secondo numero casuale tra 1 e 6, che sarà la seconda giocata.
-var dado2 = Math.floor(Math.random() * (maxValue - minValue)) + minValue
-document.getElementById('roll-datap2').innerHTML = dado2;
+// Stampa dei numeri generati.
+document.getElementById('roll-datap1').innerHTML = dadi[0];
+document.getElementById('roll-datap2').innerHTML = dadi[1];
 
 // Comunicare quali dei due utenti (tra giocatore 1 e giocatore 2) ha vinto.
-if(dado1 > dado2) {
+if(dadi[0] > dadi[1]) {
     document.getElementById('vincitore').innerHTML = "Il vincitore é il lanciatore del primo dado";
-} else if(dado1 < dado2) {
+} else if(dadi[0] < dadi[1]) {
     document.getElementById('vincitore').innerHTML = "Il vincitore é il lanciatore del secondo dado";
 } else {
     document.getElementById('vincitore').innerHTML = "Pareggio! Ricaricare la pagina per riprovare...";
