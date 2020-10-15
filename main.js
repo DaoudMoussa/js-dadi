3354693104084160ebf1992cf50b2c909301b9ba
Daoud <99m.daoud@gmail.com>
2020-10-15 10:23:25 +0200

    Corretto range - inserita stampa nel for

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 const minValue = 1;
-const maxValue = 6;
+const maxValue = 7;
 var dadi = [];
 
 // Generare 2 numeri casuali tra 1 e 6 per simulare il lancio di 2 dadi.
 for (var i = 0; i < 2; i++) {
     dadi.push(Math.floor(Math.random() * (maxValue - minValue)) + minValue);
-    console.log(dadi[i]);
+    document.getElementById('roll-datap' + (i + 1)).innerHTML = dadi[i];
 }
-
-// Stampa dei numeri generati.
-document.getElementById('roll-datap1').innerHTML = dadi[0];
-document.getElementById('roll-datap2').innerHTML = dadi[1];
 
 // Comunicare quali dei due utenti (tra giocatore 1 e giocatore 2) ha vinto.
 if(dadi[0] > dadi[1]) {
